8e91e5a77388f4b134dda395896191b66ae1b74d
mtpauly <peregrine.pauly@gmail.com>
2024-06-10 17:24:29 -0500

    Chapter 1

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"monkey/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Type in commands here\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
